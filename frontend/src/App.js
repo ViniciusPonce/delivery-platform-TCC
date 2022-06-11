11899687bf145c4e149f863ee2eb25b8d7080341
Vinicius Ponce <vinicius.ponce@hotmail.com>
2022-06-10 23:25:08 -0300

    Test request

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
+import api from "./services/api";
 
 function App() {
+
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    api.get("/user")
+      .then((response) => setUser(response.data))
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+  }, []);
+  console.log(user)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {user}
         </p>
         <a
           className="App-link"
