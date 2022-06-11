335b9b962bb173479a4d130aa6700a32259dcff0
Vinicius Ponce <vinicius.ponce@hotmail.com>
2022-06-10 23:23:38 -0300

    Test routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,6 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group([
+    'middleware' => [
+        'api',
+        'cors'
+    ],
+], function ($routes) {
+
+    Route::get('/user', function (Request $request) {
+        return 'teste';
+    });
+
 });
