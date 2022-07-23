cb668031eab3c56f0bf3934429d8b09086c30f72
talyseugenio <talysebf@gmail.com>
2022-07-22 23:52:08 -0300

    Add Mask

--- frontend/src/components/RegistrationData.js
+++ frontend/src/components/RegistrationData.js
@@ -1,25 +1,69 @@
 import React from 'react';
+import { IMaskInput } from 'react-imask';
 import Form from 'react-bootstrap/Form';
 
 const RegistrationData = () => {
 
 
     return (
-        <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail" autoComplete="off">
-          <Form.Control type="email" placeholder="Informe seu E-mail" />
+      <Form>
+        <Form.Group 
+          className="mb-3"
+          controlId="formBasicEmail" 
+          >
+            <Form.Control 
+              type="email" 
+              autoComplete="off"
+              placeholder="Informe seu E-mail"
+              as={IMaskInput}
+              mask={/^\S*@?\S*$/} 
+              />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword" autoComplete="off">
-          <Form.Control type="nameLastName" placeholder="Nome e Sobrenome" />
+        <Form.Group 
+          className="mb-3" 
+          controlId="formBasicNameLastName" 
+          >
+            <Form.Control 
+              type="nameLastName"
+              autoComplete="off"  
+              placeholder="Nome e Sobrenome" 
+              />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword" autoComplete="off">
-          <Form.Control type="cpf" placeholder="Informe seu CPF" />
+        <Form.Group 
+          className="mb-3" 
+          controlId="formBasicCpf" 
+          >
+            <Form.Control 
+              type="cpf" 
+              autoComplete="off"
+              placeholder="Informe seu CPF" 
+              as={IMaskInput} 
+              mask="000.000.000-00"
+              />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword" autoComplete="off">
-          <Form.Control type="birthDate" placeholder="Data de Nascimento" autoComplete="off"/>
+        <Form.Group 
+          className="mb-3" 
+          controlId="formBasicBirthDate" 
+          >
+            <Form.Control 
+              type="birthDate" 
+              placeholder="Data de Nascimento" 
+              autoComplete="off" 
+              as={IMaskInput} 
+              mask="00/00/0000"
+              />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword" autoComplete="off">
-          <Form.Control type="phone" placeholder="Telefone Ex: 99999-9999" />
+        <Form.Group 
+          className="mb-3" 
+          controlId="formBasicPhone" 
+          >
+            <Form.Control 
+              type="phone" 
+              autoComplete="off"
+              placeholder="Telefone Ex: 99999-9999" 
+              as={IMaskInput} 
+              mask={"(00) 0 0000-0000"}
+              />
         </Form.Group>
       </Form>
     )
