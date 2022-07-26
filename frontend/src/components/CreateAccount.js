e97a0175562c2828ea5669d3ecdb1d3e288dcb93
Talys Eugênio <talysebf@gmail.com>
2022-07-25 22:54:32 -0300

    Struct Form CreateAccount

--- frontend/src/components/CreateAccount.js
+++ frontend/src/components/CreateAccount.js
@@ -1,49 +1,40 @@
-// import Button from '@mui/material/Button'
-// import Modal from '@mui/material/Modal'
-// import Box from '@mui/material/Box'
-// import Typography from '@mui/material/Typography'
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { useState } from 'react'
+
 import RegistrationData from './RegistrationData'
 
 import '../styles/ButtonText.css'
 
-const CreateAccount = () => {
+const CreateAccount = ({email, nameLastName, cpf, birthDate, phone}) => {
 
-    // const [open, setOpen] = useState(false)
     const [show, setShow] = useState(false);
-    // const handleOpen = () => setOpen(true)
-    // const handleClose = () => setOpen(false)
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
     return (
-      <>
-        <Button variant="light" onClick={handleShow}>
+        <>
+        <Button variant="link" onClick={handleShow} style={{boxShadow: 'none'}}>
             Criar Conta      
         </Button>
 
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Falta pouco</Modal.Title>
-            {/* <Modal.Subtitle>Preencha com seus dados para criar uma conta</Modal.Subtitle> */}
-          </Modal.Header>
-
-          <RegistrationData/>        
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Cancelar
-            </Button>
-
-            <Button variant="primary" onClick={handleClose}>
-              Continuar
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Falta pouco</Modal.Title>
+        </Modal.Header>
+        <RegistrationData/>        
+        <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+            Cancelar
+          </Button>
+          <Button variant="primary">
+            Continuar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+        </>
     )
 }
 
--- frontend/src/components/RegistrationData.js
+++ frontend/src/components/RegistrationData.js
@@ -1,32 +1,106 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { IMaskInput } from 'react-imask';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const RegistrationData = () => {
-  return (
-    <Form>
-      
-      <Form.Group className="mb-3" controlId="formBasicEmail" autoComplete="off">
-        <Form.Control type="email" placeholder="Informe seu E-mail" />
-      </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword" autoComplete="off">
-        <Form.Control type="nameLastName" placeholder="Nome e Sobrenome" />
-      </Form.Group>
+  const [email, setEmail] = useState('')
+  const [nameLastName, setNameLastName] = useState('')
+  const [cpf, setCpf] = useState('')
+  const [birthDate, setBirthDate] = useState('')
+  const [phone, setPhone] = useState('')
 
-      <Form.Group className="mb-3" controlId="formBasicPassword" autoComplete="off">
-        <Form.Control type="cpf" placeholder="Informe seu CPF" />
-      </Form.Group>
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('Formulário enviado')
+    console.log(`email: ${email},\nnameLastName: ${nameLastName},\ncpf: ${cpf},\nbirthDate: ${birthDate},\nphone: ${phone}`)
 
-      <Form.Group className="mb-3" controlId="formBasicPassword" autoComplete="off">
-        <Form.Control type="birthDate" placeholder="Data de Nascimento" autoComplete="off"/>
-      </Form.Group>
+    setEmail('')
+    setNameLastName('')
+    setCpf('')
+    setBirthDate('')
+    setPhone('')
+  }
 
-      <Form.Group className="mb-3" controlId="formBasicPassword" autoComplete="off">
-        <Form.Control type="phone" placeholder="Telefone Ex: 99999-9999" />
-      </Form.Group>
-
-    </Form>
-  )
+    return (
+      <div>
+        <Form onSubmit={handleSubmit}>
+          <Form.Label>Preencha com seus dados para criar uma conta</Form.Label>
+          <Form.Group 
+            className="mb-3"
+            controlId="formBasicEmail" 
+            >
+              <Form.Control 
+                type="email" 
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                autoComplete="off"
+                placeholder="Informe seu E-mail: "
+                as={IMaskInput}
+                mask={/^\S*@?\S*$/} 
+                />
+          </Form.Group>
+          <Form.Group 
+            className="mb-3" 
+            controlId="formBasicNameLastName" 
+            >
+              <Form.Control 
+                type="nameLastName"
+                onChange={(e) => setNameLastName(e.target.value)}
+                value={nameLastName}
+                autoComplete="off"  
+                placeholder="Nome e Sobrenome: " 
+                />
+          </Form.Group>
+          <Form.Group 
+            className="mb-3" 
+            controlId="formBasicCpf" 
+            >
+              <Form.Control 
+                type="cpf" 
+                value={cpf}
+                onChange={(e) => setCpf(e.target.value)}
+                autoComplete="off"
+                placeholder="Informe seu CPF: " 
+                as={IMaskInput} 
+                mask="000.000.000-00"
+                />
+          </Form.Group>
+          <Form.Group 
+            className="mb-3" 
+            controlId="formBasicBirthDate" 
+            >
+              <Form.Control 
+                type="birthDate" 
+                value={birthDate}
+                onChange={(e) => setBirthDate(e.target.value)}
+                placeholder="Data de Nascimento: " 
+                autoComplete="off" 
+                as={IMaskInput} 
+                mask="00/00/0000"
+                />
+          </Form.Group>
+          <Form.Group 
+            className="mb-3" 
+            controlId="formBasicPhone" 
+            >
+              <Form.Control 
+                type="phone" 
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+                autoComplete="off"
+                placeholder="Telefone: " 
+                as={IMaskInput} 
+                mask={"(00) 0 0000-0000"}
+                />
+          </Form.Group>
+            <Button variant="primary" type="submit">
+              Continuar
+            </Button>
+        </Form>
+      </div>
+    )
 }
 
 export default RegistrationData
