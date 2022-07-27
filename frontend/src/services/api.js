d0337e7d3a04e5245fb534d4b4cb17bc54b0f91a
Vinicius Ponce <vinicius.ponce@hotmail.com>
2022-07-26 22:04:23 -0300

    Adjust environments

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,18 @@
 import axios from "axios";
 
+// Retire o comentario para utilizar da forma em que deseja 
+// em ambiente de desenvolvimento
+
+// Cliente para acesso em localhost
+// const api = axios.create({
+//     baseURL: "http://"+ process.env.LOCAL_SERVER_ACCESS_ROUTING + ":8000/api",
+// });
+
+
+// Client para rotear acesso server local
+// OBS: Adicionar ip_address ipv4 no .env.development
 const api = axios.create({
-    baseURL: "http://192.168.1.3:8000/api",
+    baseURL: "http://"+ process.env.LOCAL_SERVER_ACCESS_ROUTING + ":8000/api",
 });
 
 export default api;
